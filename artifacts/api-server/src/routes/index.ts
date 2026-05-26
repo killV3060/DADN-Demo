869867ff90e858d49b39ae0c0e1c8108a3c84fae
Vinh <phuvi301@gmail.com>
2026-05-27 00:59:13 +0700

    fix: rollback to old change

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -8,7 +8,7 @@ const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(authenticateOptional);
-router.use(authRouter);
+router.use("/auth", authRouter);
 router.use(iotRouter);
 
 export default router;
